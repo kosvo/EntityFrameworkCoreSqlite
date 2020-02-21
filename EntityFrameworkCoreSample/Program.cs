31f9023152808b87955fc437d7c31a8968d2fa52
Kostiantyn Voitenko <>
2020-02-21 10:56:46 +0200

    init project with bulk operations benchmark

---
+++ EntityFrameworkCoreSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameworkCoreSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
